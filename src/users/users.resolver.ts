44c10afa54572f0a47b7d17bfe6af126a26a1684
ofadiman <ofadiman@gmail.com>
2021-11-13 13:07:19 +0100

    feat: add upvotePost mutation

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,21 +1,38 @@
-import { Args, Int, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
+import { Args, Field, InputType, Int, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
 
 import { Post } from '../posts/post.model'
 import { PostsService } from '../posts/posts.service'
 import { User } from './user.model'
 import { UsersService } from './users.service'
 
+@InputType()
+export class UpvotePostArgs {
+  @Field({ description: `The ID of the post to be updated.` })
+  public id: number
+}
+
 @Resolver((): typeof User => User)
 export class UsersResolver {
   public constructor(private readonly usersService: UsersService, private readonly postsService: PostsService) {}
 
-  @Query((): typeof User => User, { name: `user` })
+  @Query((): typeof User => User, { description: `The query returns the user with the selected ID.`, name: `user` })
   public async readOneUserById(@Args(`id`, { type: (): typeof Int => Int }) id: number): Promise<User> {
     return this.usersService.readOneById({ id })
   }
 
-  @ResolveField(`posts`, (): [typeof Post] => [Post])
+  @ResolveField(`posts`, (): [typeof Post] => [Post], {
+    description: `A list of posts that have been created by the user with the selected ID.`
+  })
   public async readManyPostsByUserId(@Parent() user: User): Promise<Array<Post>> {
     return this.postsService.readAllByUserId({ id: user.id })
+  }
+
+  @Mutation((): typeof Post => Post)
+  public async upvotePost(@Args(UpvotePostArgs.name) upvotePostArgs: UpvotePostArgs): Promise<Post> {
+    return {
+      id: upvotePostArgs.id,
+      title: `test`,
+      votes: 1
+    }
   }
 }
